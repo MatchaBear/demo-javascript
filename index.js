18b18c6bcf636bbc6da17a8375ba52db3f6a99c3
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-01-15 10:54:08 +0800

    fix: scissor array value from showing

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const grassHeight = prompt(
   "how tall (in centimeter) is your grass height right now ?"
 );
 
-const scissor = [{ size: small }, { size: medium }, { size: big }];
+const scissor = [{ size: "small" }, { size: "medium" }, { size: "big" }];
 
 if (grassHeight > 1 && grassHeight <= 10) {
-  useScissor = scissor.size[0];
-  console.log(`You can cut the grass with $(useScissor)`);
+  useScissor = scissor[0].size;
+  console.log(`You can cut the grass with ${useScissor} scissor`);
 } else if (grassHeight <= 50) {
-  useScissor = scissor.size[1];
-  console.log(`You can cut the grass with $(useScissor)`);
+  useScissor = scissor[1].size;
+  console.log(`You can cut the grass with ${useScissor} scissor`);
 } else if (grassHeight <= 100) {
-  useScissor = scissor.size[2];
-  console.log(`You can cut the grass with $(useScissor)`);
+  useScissor = scissor[2].size;
+  console.log(`You can cut the grass with ${useScissor} scissor`);
 } else if (grassHeight > 100) {
   console.log(`Grass height is too long!`);
 } else {
